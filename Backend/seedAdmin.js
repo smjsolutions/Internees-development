a720f16ce206c10079558ce0442abdec04f04c9e
Laiba Waheed <laibawaheed122@gmail.com>
2026-01-26 19:03:55 +0500

    Resolved issues

--- Backend/seedAdmin.js
+++ Backend/seedAdmin.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 require("dotenv").config();
 const connectDB = require("./src/config/db");
 const AdminUser = require("./src/models/adminUser.model.js");
